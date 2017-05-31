73c596e717325cdaf7f54f8f4b897080324efd74
Ravi Srinivasan <rsriniva@redhat.com>
2017-05-31 23:41:32 +0530

    simple JAX-RS microservice deployed to openshift using the fabric8 plugin

---
+++ src/main/java/com/redhat/training/openshift/hello/HelloResource.java
@@ -0,0 +1,25 @@
+package com.redhat.training.openshift.hello;
+
+import javax.inject.Inject;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.Produces;
+
+@Path("/")
+public class HelloResource {
+
+    @GET
+    @Path("/hello")
+    @Produces("text/plain")
+    public String hola() {
+        String hostname = System.getenv().getOrDefault("HOSTNAME", "unknown");
+        return "Hello world from host ["+hostname+"]\n";
+    }
+
+    @GET
+    @Path("/health")
+    @Produces("text/plain")
+    public String health() {
+        return "OK\n";
+    }
+}
